60fc556a673d3d7ff94229b80005c8f07a1afac3
unknown <kylevh7@gmail.com>
2018-10-06 09:42:50 -0400

    rendering correct books, titles and authors on shelves

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -5,7 +5,7 @@ render() {
         <li>
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+              <div className="book-cover" style={{ width: 128, height: 188, backgroundImage:`url(${this.props.imageLinks.thumbnail})`||`${this.props.title} image is unavailable` }}></div>
               <div className="book-shelf-changer">
                 <select>
                   <option value="move" disabled>Move to...</option>
@@ -16,8 +16,8 @@ render() {
                 </select>
               </div>
             </div>
-            <div className="book-title">Ender's Game</div>
-            <div className="book-authors">Orson Scott Card</div>
+            <div className="book-title">{this.props.title}</div>
+            <div className="book-authors">{this.props.authors}</div>
           </div>
         </li>
     );
